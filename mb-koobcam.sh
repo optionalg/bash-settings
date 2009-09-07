426f159d1869e796a37e8aae017fd89d0e4e7fb8
Rob Friedman <px@ns1.net>
2009-09-07 00:27:27 -0700

    pretty prompt

--- mb-koobcam.sh
+++ mb-koobcam.sh
@@ -6,11 +6,11 @@ dir=`dirname $BASH_SOURCE`
 . $dir/common/terminal.sh
 
 #export PS1='\[\033[0;37m\]\w\[\033[00m\] \$ ' # override PS1 to a simpler format
-
 # default
 #export PS1='\h:\W \u\$'
 
-export PS1="\[\e[0;31m\]\u@\h:\w\$ \[\e[1;30m\]"
+export PS1='\[\033[01;33m\]\u@\h\[\033[01;31m\] \w \[\033[01;34m\]\$\[\033[00m\] '
+#export PS1="\[\e[0;31m\]\u@\h:\w\$ \[\e[1;30m\]"
 
 . $dir/common/path.sh
 . $dir/common/misc-env.sh
