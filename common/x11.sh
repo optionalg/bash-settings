08f32839f20fbb38a0fea7b5dba674409d5b9083
Rob Friedman <px@ns1.net>
2010-04-29 22:48:09 -0700

    system specific

--- common/x11.sh
+++ common/x11.sh
@@ -1,6 +1,7 @@
-
+if [[ $mySystem == "Darwin" ]]; then
   if [ -z "$DISPLAY" ]; then
-    if [ `ps -awux|grep  X11.app |grep -vc grep` -gt 0 ]; then
+    if [ `ps -awwwux | grep  X11.app | grep -vc grep` -gt 0 ]; then
       export DISPLAY=":0.0"
     fi
   fi
+fi
