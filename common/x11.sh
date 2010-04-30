45a8a163387c563f03373cf7fb5c24c2987722bf
Rob Friedman <px@ns1.net>
2010-04-29 22:46:21 -0700

    fix

--- common/x11.sh
+++ common/x11.sh
@@ -1,6 +1,6 @@
 
   if [ -z "$DISPLAY" ]; then
-    if [ `ps -awwwux|grep  X11.app |grep -vc grep` -gt 0 ]; then
+    if [ `ps -awux|grep  X11.app |grep -vc grep` -gt 0 ]; then
       export DISPLAY=":0.0"
     fi
   fi
