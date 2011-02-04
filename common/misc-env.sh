eb05b39945f9a4d5e2b4f3fe4a76adbb1b935840
Nikita Afanasenko <nikita@afanasenko.name>
2011-02-04 09:05:35 +0300

    move folder aliases

--- common/misc-env.sh
+++ common/misc-env.sh
@@ -2,3 +2,4 @@ export GREP_OPTIONS='--color=auto'
 export LS_OPTIONS=' -G --color=auto'
 
 alias f5='source ~/.bashrc'
+alias .bash='cd ~/.bash'
--- desktop/work.sh
+++ desktop/work.sh
@@ -1,5 +1,1 @@
-alias work='     cd ~/work'
-alias projects=' work; cd projects'
-
-alias .bash='   cd ~/.bash'
-
+alias work='cd ~/work'
