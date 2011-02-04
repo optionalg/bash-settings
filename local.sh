03b8997acd7d88d4e994a2009206a5318d15c5fb
Nikita Afanasenko <nikita@afanasenko.name>
2011-02-04 09:28:29 +0300

    add submodules initialization

--- local.sh
+++ local.sh
@@ -20,8 +20,6 @@ export PS1='\[\033[0;37m\]\w\[\033[00m\] \$ ' # override PS1 to a simpler format
 . $dir/common/git.sh
 #. $dir/common/git-one-letter-aliases.sh
 
-if [ -f $dir/hosts/hosts.sh ]; then
-    . $dir/hosts/hosts.sh
-fi
+. $dir/submodules.sh
 
 test -r /sw/bin/init.sh && . /sw/bin/init.sh
