ecd4294b42a2c3bbd6affafe760010f56578dab9
Nikita Afanasenko <nikita@afanasenko.name>
2011-02-23 13:44:29 +0300

    desktop aliases

--- local.sh
+++ local.sh
@@ -15,6 +15,7 @@ export PS1='\[\033[0;37m\]\w\[\033[00m\] \$ ' # override PS1 to a simpler format
 #. $dir/common/git-one-letter-aliases.sh
 
 . $dir/desktop/work.sh
+. $dir/desktop/aliases.sh
 
 . $dir/submodules.sh
 
