4790bcef213973493a38603c3b653d1f763f2dbd
Nikita Afanasenko <nikita@afanasenko.name>
2011-08-03 13:49:21 +0400

    add taskwarrior aliases

--- desktop/aliases.sh
+++ desktop/aliases.sh
@@ -2,3 +2,9 @@ alias chrome=google-chrome
 
 function p { cd ~/projects/$1; }
 function vp { p $1 && gvim .; }
+
+# task aliases
+#
+alias t=task
+alias ts='task ls'
+function tsp { task pro:$1 ls; }
