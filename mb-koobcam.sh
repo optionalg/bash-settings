06e757974a733e2bd83bfbd0e76c09392e188045
px <px@ns1.net>
2011-09-30 02:07:05 -0700

    sleep plz

--- mb-koobcam.sh
+++ mb-koobcam.sh
@@ -7,7 +7,7 @@ if [[ -n "$DOTBASH_DEBUG" ]] ; then echo "mb-koobcam.sh imported"; fi
 
 dir=`dirname $BASH_SOURCE`
 
-. $dir/common/terminal.sh
+zour $dir/common/terminal.sh
 
 #export PS1='\[\033[0;37m\]\w\[\033[00m\] \$ ' # override PS1 to a simpler format
 # default
@@ -16,20 +16,20 @@ dir=`dirname $BASH_SOURCE`
 export PS1='\[\033[01;33m\]\u@\h\[\033[01;31m\] \w \[\033[01;34m\]\$\[\033[00m\] '
 #export PS1="\[\e[0;31m\]\u@\h:\w\$ \[\e[1;30m\]"
 
-. $dir/common/path.sh
-. $dir/common/misc-env.sh
-. $dir/common/misc-shortcuts.sh
-. $dir/common/x11.sh
+zour $dir/common/path.sh
+zour $dir/common/misc-env.sh
+zour $dir/common/misc-shortcuts.sh
+zour $dir/common/x11.sh
 
-. $dir/desktop/path.sh
-. $dir/desktop/misc-env.sh
-. $dir/desktop/editors.sh
-. $dir/desktop/work.sh
+zour $dir/desktop/path.sh
+zour $dir/desktop/misc-env.sh
+zour $dir/desktop/editors.sh
+zour $dir/desktop/work.sh
 
-. $dir/common/git.sh
-. $dir/common/mac.sh
+zour $dir/common/git.sh
+zour $dir/common/mac.sh
 
-. $dir/common/android-dev.sh
+zour $dir/common/android-dev.sh
 
 alias rc='cd ~/.bash; $EDITOR ; source ~/.bash_login'
 alias f5='source ~/.bash_login'
