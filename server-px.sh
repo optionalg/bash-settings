c801127c13233c6e691488c2989761bfcbccbf89
Rob Friedman <px@ns1.net>
2011-10-09 18:01:31 -0700

    server-px updates

--- server-px.sh
+++ server-px.sh
@@ -1,33 +1,33 @@
 #!/bin/echo "This file is meant to be sourced from the command or .bashrc"\
 # Usage:
-#   echo ". ~/.bash/server.sh" >> ~/.bash_login
+#   echo "zour ~/.bash/server.sh" >> ~/.bash_login
 
 ## default PS1 on gentoo
 export PS1='\[\033[01;32m\]\u@\h\[\033[01;34m\] \w \$\[\033[00m\] '
 
 dir=`dirname $BASH_SOURCE`
 
-. $dir/common/terminal.sh
-. $dir/common/path.sh
-. $dir/common/misc-env.sh
-. $dir/common/misc-shortcuts.sh
-. $dir/common/ssh.sh
-. $dir/common/x11.sh
+zour $dir/common/terminal.sh
+zour $dir/common/path.sh
+zour $dir/common/misc-env.sh
+zour $dir/common/misc-shortcuts.sh
+zour $dir/common/ssh.sh
+zour $dir/common/x11.sh
 
-. $dir/common/linux.sh
-. $dir/common/lvm.sh
-. $dir/common/md.sh
+zour $dir/common/linux.sh
+zour $dir/common/lvm.sh
+zour $dir/common/md.sh
 
-. $dir/common/gentoo.sh
-. $dir/common/ftp.sh
+zour $dir/common/gentoo.sh
+zour $dir/common/ftp.sh
 
-. $dir/server/range.sh
-. $dir/server/misc.sh
+zour $dir/server/range.sh
+zour $dir/server/misc.sh
 
-. $dir/server/editors.sh
-. $dir/server/ejabber.sh 
+zour $dir/server/editors.sh
+zour $dir/server/ejabber.sh 
 
-. $dir/common/git.sh
+zour $dir/common/git.sh
 
-test -r /sw/bin/init.sh && . /sw/bin/init.sh
+#test -r /sw/bin/init.sh && zour /sw/bin/init.sh
 
