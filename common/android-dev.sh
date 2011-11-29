2d59c34513e961603f008bc50839a021be098623
px <px@ns1.net>
2011-11-29 15:17:12 -0800

    hmm

--- common/android-dev.sh
+++ common/android-dev.sh
@@ -7,4 +7,13 @@
 # set the number of open files to be 1024
 #ulimit -S -n 1024
 
-PATH=/Users/backup/Downloads/2011/xda/Samsung/android-sdk-mac_x86/platform-tools:$PATH
+
+TOOLDIR="/Users/backup/Downloads/2011/android.com/android-sdk-macosx/tools"
+if [ -d "${TOOLDIR}" ]; then
+	PATH="${TOOLDIR}:${PATH}"
+fi
+
+PLATTOOLDIR="/Users/backup/Downloads/2011/android.com/android-sdk-macosx/platform-tools"
+if [ -d "${PLATTOOLDIR}" ]; then
+	PATH="${PLATTOOLDIR}:${PATH}"
+fi
