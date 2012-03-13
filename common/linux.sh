6a2009bc2d8208168a5fa77f2026f4091c5ff1fd
Rob Friedman <px@ns1.net>
2012-03-13 14:54:37 -0700

    progress

--- common/linux.sh
+++ common/linux.sh
@@ -24,8 +24,7 @@ alias eth0="ifconfig eth0"
 ## save iptables state
 
 ## fix ip_save when done
-alias QuickBlock="ipt_save && ipt_log ${@} && ipt_drop ${@} && ip_save"
-##
+# #
 ## log this ip
 ## block this ip
 
@@ -33,11 +32,21 @@ alias QuickBlock="ipt_save && ipt_log ${@} && ipt_drop ${@} && ip_save"
 alias ipt_save="$NOT_ROOT /etc/init.d/iptables save"
 ## email
 
-NOT_ROOT=echo
+echo $NOT_ROOT
+NOT_ROOT=`which sudo`
 ## [-b bcc-addr] [-c cc-addr] [-s subject]            to-addr
-alias ipt_email="mail -c logs+iptables@ns1.net -s \"bad stuff\" px@ns1.net"
-alias ipt_log="$NOT_ROOT iptables -A INPUT -s ${1} -m limit --limit 5/hour -j LOG --log-prefix \"${2} ${1}\""
-alias ipt_drop="$NOT_ROOT iptables -A INPUT -s ${1} -j DROP"
+function ipt_email() { mail -c logs+iptables@ns1.net -s \"bad stuff\" px@ns1.net ; }
+function ipt_log() { 
+
+# fix log-prefix to work within string limits
+$NOT_ROOT echo iptables -A INPUT -s ${1} -m limit --limit 5/hour -j LOG --log-prefix \"${@}\" ; }
+#
+function ipt_drop() {
+ $NOT_ROOT echo  iptables -A INPUT -s ${1} -j DROP ;
+ }
 
 #[9209:519904] -A INPUT -s 220.181.0.0/16 -m limit --limit 5/hour -j LOG --log-prefix "baiduhttp abuse 220.181.0.0/1"
 #[15402:858484] -A INPUT -s 220.181.0.0/16 -j DROP
+
+
+function  QuickBlock() { ipt_save && ipt_log $@ && ipt_drop $@ && ip_save ; }
