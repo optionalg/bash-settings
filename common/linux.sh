d40c91aa9322346603040c152ecc81c4a50461fb
Rob Friedman <px@ns1.net>
2012-03-13 14:09:33 -0700

    in-progress

--- common/linux.sh
+++ common/linux.sh
@@ -16,3 +16,23 @@ alias ath0="ifconfig ath0"
 alias eth0="ifconfig eth0"
 
 
+### whois ip, email
+
+## save iptables state
+## add log 5/hr
+## drop/block ip
+## save iptables state
+## 208.80.194.30
+## 95.108.150.235
+alias QuickBlock=""
+##
+## log this ip
+## block this ip
+
+## save iptables state
+## email
+alias ipt_log="$not_root iptables -A INPUT -s $1 -m limit --limit 5/hour -j LOG --log-prefix \"$2* $1\""
+alias ipt_drop="$not_root iptables -A INPUT -s $1 -j DROP"
+
+#[9209:519904] -A INPUT -s 220.181.0.0/16 -m limit --limit 5/hour -j LOG --log-prefix "baiduhttp abuse 220.181.0.0/1"
+#[15402:858484] -A INPUT -s 220.181.0.0/16 -j DROP
