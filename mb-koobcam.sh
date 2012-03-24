dfb7aef59b50e7d2ed8c27913b255c6094853a8c
px <px@ns1.net>
2012-03-23 22:39:56 -0700

    ok

--- mb-koobcam.sh
+++ mb-koobcam.sh
@@ -17,6 +17,7 @@ export PS1='\[\033[01;33m\]\u@\h\[\033[01;31m\] \w \[\033[01;34m\]\$\[\033[00m\]
 #export PS1="\[\e[0;31m\]\u@\h:\w\$ \[\e[1;30m\]"
 
 zour "$dir/common/path.sh"
+zour "$dir/common/android-dev.sh"
 zour "$dir/common/misc-env.sh"
 zour "$dir/common/misc-shortcuts.sh"
 #zour "$dir/common/x11.sh"
@@ -29,7 +30,6 @@ zour "$dir/desktop/work.sh"
 zour "$dir/common/git.sh"
 zour "$dir/mac.sh"
 
-zour "$dir/common/android-dev.sh"
 
 alias rc='cd ~/.bash; $EDITOR ; source ~/.bash_login'
 alias f5='source ~/.bash_login'
