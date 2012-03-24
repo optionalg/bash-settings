3b511b7691a0169b94eba5cb05a74b54ea601aa3
px <px@ns1.net>
2012-03-23 22:39:56 -0700

    ok

--- common/android-dev.sh
+++ common/android-dev.sh
@@ -7,13 +7,12 @@
 # set the number of open files to be 1024
 #ulimit -S -n 1024
 
-
-TOOLDIR="/Users/backup/Downloads/2011/android.com/android-sdk-macosx/tools"
+TOOLDIR="/Users/backup/Downloads/2012/android-sdk-macosx/tools"
 if [ -d "${TOOLDIR}" ]; then
 	PATH="${TOOLDIR}:${PATH}"
 fi
 
-PLATTOOLDIR="/Users/backup/Downloads/2011/android.com/android-sdk-macosx/platform-tools"
+PLATTOOLDIR="/Users/backup/Downloads/2012/android-sdk-macosx/platform-tools"
 if [ -d "${PLATTOOLDIR}" ]; then
 	PATH="${PLATTOOLDIR}:${PATH}"
 fi
