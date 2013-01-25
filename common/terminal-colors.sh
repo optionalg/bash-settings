84c28692cfcfeba6a20ea551b79c6347b35276fb
px <px@ns1.net>
2013-01-25 15:02:56 -0800

    colorful

---
+++ common/terminal-colors.sh
@@ -0,0 +1,26 @@
+
+
+
+# Base styles and color palette
+# Solarized colors
+# https://github.com/altercation/solarized/tree/master/iterm2-colors-solarized
+export BOLD=$(tput bold)
+export RESET=$(tput sgr0)
+export SOLAR_YELLOW=$(tput setaf 136)
+export SOLAR_ORANGE=$(tput setaf 166)
+export SOLAR_RED=$(tput setaf 124)
+export SOLAR_MAGENTA=$(tput setaf 125)
+export SOLAR_VIOLET=$(tput setaf 61)
+export SOLAR_BLUE=$(tput setaf 33)
+export SOLAR_CYAN=$(tput setaf 37)
+export SOLAR_GREEN=$(tput setaf 64)
+export SOLAR_WHITE=$(tput setaf 254)
+
+#style_user="\[${RESET}${SOLAR_ORANGE}\]"
+#style_host="\[${RESET}${SOLAR_YELLOW}\]"
+#style_path="\[${RESET}${SOLAR_GREEN}\]"
+#style_chars="\[${RESET}${SOLAR_WHITE}\]"
+#style_branch="${SOLAR_CYAN}"
+
+
+
