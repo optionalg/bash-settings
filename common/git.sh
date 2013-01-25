1d959a5637e46852ef26aa48bd8a35213eb91222
px <px@ns1.net>
2013-01-25 15:10:57 -0800

    gitstuff

--- common/git.sh
+++ common/git.sh
@@ -1,10 +1,12 @@
 #!/bin/echo "This file is meant to be sourced from the command or .bashrc"
 
-dir=`dirname "$BASH_SOURCE"`
-_dir="$dir" 
-
-zour "$dir/bash-completion/git-completion.sh"
-zour "$dir/git-shortcuts.sh"
+#dir=`dirname "$BASH_SOURCE"`
+#_dir="$dir" 
+echo $dir
+zour "$dir/common/bash-completion/git-completion.sh"
+echo $dir
+zour "$dir/common/git-shortcuts.sh"
+echo $dir
 
 ##export PS1="\[\033[0;33m\]\$(__git_ps1 '%s ')\[\033[00m\]$PS1"
 
