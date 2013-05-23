145f8fb1ce01e521f93df81dd150143122a2db1c
px <px@ns1.net>
2013-05-23 14:46:49 -0700

    hide that

--- mb-computer.sh
+++ mb-computer.sh
@@ -7,7 +7,7 @@ if [[ -n "$DOTBASH_DEBUG" ]] ; then echo "mb-koobcam.sh imported"; fi
 
 export dir=`dirname "$BASH_SOURCE"`
 #readonly dir
-echo $dir
+#echo $dir
 
 zour "$dir/common/terminal.sh"
 ##zour "$dir/common/bash-completion/git-completion.sh"
@@ -17,30 +17,30 @@ zour "$dir/common/terminal.sh"
 
 #####export PS1='\[\033[01;33m\]\u@\h\[\033[01;31m\] \w \[\033[01;34m\]\$\[\033[00m\] '
 #export PS1="\[\e[0;31m\]\u@\h:\w\$ \[\e[1;30m\]"
-echo $dir
+#echo $dir
 zour "$dir/common/mac.sh"
-echo $dir
+#echo $dir
 zour "$dir/common/path.sh"
-echo $dir
+#echo $dir
 zour "$dir/common/android-dev.sh"
-echo $dir
+#echo $dir
 zour "$dir/common/misc-env.sh"
-echo $dir
+#echo $dir
 zour "$dir/common/misc-shortcuts.sh"
 #zour "$dir/common/x11.sh"
-echo $dir
+#echo $dir
 
 zour "$dir/desktop/path.sh"
-echo $dir
+#echo $dir
 zour "$dir/desktop/misc-env.sh"
-echo $dir
+#echo $dir
 zour "$dir/desktop/editors.sh"
-echo $dir
+#echo $dir
 zour "$dir/desktop/work.sh"
-echo $dir
+#echo $dir
 
 zour "$dir/common/git.sh"
-echo $dir
+#echo $dir
 zour "$dir/common/bash-prompt.sh"
 
 alias rc='cd ~/.bash; $EDITOR ; source ~/.bash_login'
